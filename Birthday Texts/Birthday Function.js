c2de519dad1e8555d4059494995ddad39eda9abb
cjaredm <cjared.mortenson@gmail.com>
2017-07-04 23:35:14 -0600

    Refactored old projects

--- Birthday Texts/Birthday Function.js
+++ Birthday Texts/Birthday Function.js
@@ -1,25 +1,38 @@
+//Sheet variables to make things easy.
+var ss = SpreadsheetApp.getActive();
+var births = ss.getSheetByName('Birthdays');
+
 function birthdays() {
 
-    //Breakdown of Sheets
-    const ss = SpreadsheetApp.getActive();
-    const births = ss.getSheetByName('Birthdays');
+    //Dates and formats
+    const dateFormat = "MM/dd";
+    var dateToday = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd");
+    var lastRow = births.getLastRow();
 
-    const dateToday = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd");
-    const lastRow = births.getLastRow();
 
-    //Cycle through each row on the sheet
-    for (let i = 2; i <= lastRow; ++i) {
+    //The magic loop through all rows
+    for (var i = 2; i <= lastRow; ++i) {
 
-        //check to see if the birthdate is blank or not AND if the birthdate is today's date.
-        if (births.getRange(i, 8).isBlank() == false && dateToday == Utilities.formatDate(births.getRange(i, 7).getValue(), Session.getScriptTimeZone(), "MM/dd")) {
+        //Variables that change, can't be set as let in Google App Script
+        var targetDate = new Date(births.getRange(i, 7).getValue());
+        var nickName = births.getRange(i, 3).getValue();
+        var smsText = births.getRange(i, 8).getValue();
+        var birthdayEmail = births.getRange(i, 9).getValue();
+        var sentVerificationCell = births.getRange(i, 12);
+        var isMessageBlank = births.getRange(i, 8).isBlank();
 
-            //Email the birthday person's SMS email address my personal message on their same row.
-            MailApp.sendEmail(births.getRange(i, 9).getValue(), "", births.getRange(i, 8).getValue());
-            //This sends SMS email to me letting me know that I text someone
-            MailApp.sendEmail("5551234567@vtext.com", "", "You just text " + births.getRange(i, 3).getValue() + " \'" + births.getRange(i, 8).getValue() + "\'.");
+        const myCarrierPhoneEmail = '4357735808@vtext.com';
+        const timeZone = Session.getScriptTimeZone();
+        const timeStamp = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy hh:mm");
 
-            //This adds a message to the sheet that the text was sent and when.
-            births.getRange(i, 12).setValue("Sent " + Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy hh:mm"));
+        //Put the variables to use, if the message isn't blank and the date is their birthday
+        if (!isMessageBlank && dateToday == Utilities.formatDate(targetDate, timeZone, dateFormat)) {
+            //Send the email text to them and a reminder to myself
+            MailApp.sendEmail(birthdayEmail, "", smsText);
+            MailApp.sendEmail(myCarrierPhoneEmail, "", "You just text " + nickName + " \'" + smsText + "\'.");
+
+            //Save a timestamp to the last cell of their row so I know it sent as well.
+            sentVerificationCell.setValue("Sent " + timeStamp);
         }
     }
 }
--- Lease Reminder/LeaseReminderFN.js
+++ Lease Reminder/LeaseReminderFN.js
@@ -1,33 +1,49 @@
+//This creates a shortcut name for the active spreadsheet.  
+const ss = SpreadsheetApp.getActive().getActiveSheet();
+
 function leaseReminder() {
-    const ss = SpreadsheetApp.getActive().getActiveSheet(); //This creates a shortcut name for the active spreadsheet. 
-    const dateToday = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy"); //This creates a variable for today's date to refer to in a specific format.
-    const lastRow = ss.getLastRow(); //This finds the total number of rows in the sheet with data in it to run through or else it loops forever in the spreadsheet or breaks.
-    const template = HtmlService.createTemplateFromFile("ReminderEmail"); //This creates a variable for the HTML email we've created.
+
+    //This creates a variable for today's date to refer to in a specific format.
+    var dateToday = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy");
+    var lastRow = ss.getLastRow();
+
+    //This creates a variable for the HTML email we've created.
+    var template = HtmlService.createTemplateFromFile("ReminderEmail");
 
     //When triggered this will look for anyone it needs to email.
     for (var i = 2; i <= lastRow; ++i) {
-        if (ss.getRange(i, 9).isBlank() == false && dateToday == Utilities.formatDate(ss.getRange(i, 9).getValue(), Session.getScriptTimeZone(), "MM/dd/yyyy"))
+        var isReminderDateBlank = ss.getRange(i, 9).isBlank();
+        var reminderDate = ss.getRange(i, 9).getValue();
+        const dateFormat = "MM/dd/yyyy";
+        const timeZone = Session.getScriptTimeZone();
 
-        //Create the HTML variables for the template email.
-        {
+        if (!isReminderDateBlank && dateToday == Utilities.formatDate(reminderDate, timeZone, dateFormat))
+
+        { //Variables for the email template
+            var sentVerificationCell = ss.getRange(i, 13);
             var email = ss.getRange(i, 4).getValue();
-            template.todayDate = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy");
+            const myEmail = "cjared.mortenson@gmail.com";
+            const subject = "Your lease expires next week";
+            const subjectToMe = "Reminder Lease Email Sent";
+            var renterName = ss.getRange(i, 1).getValue();
+            const timeStampFormat = "MM/dd/yyyy hh:mm";
+            var timeStamp = Utilities.formatDate(new Date(), timeZone, timeStampFormat);
+
+            template.todayDate = Utilities.formatDate(new Date(), timeZone, dateFormat);
             template.name = ss.getRange(i, 1).getValue();
             template.street = ss.getRange(i, 2).getValue();
             template.cityState = ss.getRange(i, 3).getValue();
             template.reminderDate = ss.getRange(i, 8).getValue();
-            template.expireDate = Utilities.formatDate(ss.getRange(i, 8).getValue(), Session.getScriptTimeZone(), "EEE MMMM dd, yyyy");
+            template.expireDate = Utilities.formatDate(ss.getRange(i, 8).getValue(), timeZone, "EEE MMMM dd, yyyy");
             template.annual = ss.getRange(i, 11).getValue();
             template.monthToMonth = ss.getRange(i, 12).getValue();
 
-            //This is how you send the email in form of (Receipient, Subject, Body, attachment)
-            //This email is to the renter
-            MailApp.sendEmail(email, "Your lease expires next week", "", { htmlBody: template.evaluate().getContent(), });
-            ss.getRange(i, 13).setValue("Sent " + Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy hh:mm"));
+            //This email is to the renter          
+            MailApp.sendEmail(email, subject, "", { htmlBody: template.evaluate().getContent(), });
+            sentVerificationCell.setValue("Sent " + timeStamp);
 
-            //This email is to you letting you know the previous email was sent.
-            MailApp.sendEmail("youremail@domain.com", "Reminder Lease Email Sent", "The following Lease Expiration Reminder was just sent to " + ss.getRange(i, 1).getValue(), { htmlBody: template.evaluate().getContent(), });
-            ss.getRange(i, 13).setValue("Sent " + Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "MM/dd/yyyy hh:mm"));
+            //This email is to you letting you know the previous email was sent         
+            MailApp.sendEmail(myEmail, subjectToMe, "The following Lease Expiration Reminder was just sent to " + renterName, { htmlBody: template.evaluate().getContent(), });
         }
     }
 }
